d985fb4356c7235a389d19fee2d1873516762a5f
Ali Samhat <alisamhat2007@gmail.com>
2024-04-20 13:14:57 +0300

    Ignore more modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc', 's
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'pages/reference-architecture', 'some/other/file.txt']
-autodoc_mock_imports = ['ToBeChecked', 'Dataset', 'fcntl', 'yacs','skimage', 'rasterio', 'tqdm', 'matplotlib', 'sklearn', 'geopandas', 'pandas', 'numpy', 'torch', 'yaml']
+autodoc_mock_imports = ['cv2', 'torchvision', 'color_map', 'shapely', 'PIL', 'tabulate', 'ToBeChecked', 'Dataset', 'fcntl', 'yacs','skimage', 'rasterio', 'tqdm', 'matplotlib', 'sklearn', 'geopandas', 'pandas', 'numpy', 'torch', 'yaml']
 
 
 
